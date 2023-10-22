369fcdf33fab500d27a865e1294ffecf75cc9f68
Taylor Alfers <tayloralfers@gmail.com>
2023-10-21 20:47:12 -0600

    add comments for updates

--- chart.py
+++ chart.py
@@ -2,11 +2,11 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 import json
  
+# Update this filename to change which file to load into memory
 filename = './analysis/occupancy-rates'
 f = open(f'{filename}.json')
 data = json.load(f)
 
-# hm = sn.heatmap(data = data)
+# Update the location of the data below to choose where in your dataset to chart/visualize
 plt.hist(data['detailed']['three_bedrooms_histogram'])
 plt.savefig('./3-bed-occupancy-histogram')
-
